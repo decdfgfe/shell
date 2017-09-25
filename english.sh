2e6ec1a68d8f5f5c1cbb5f2924d263929702f3bf
decdfgfe <decdfgfe@gmail.com>
2017-09-25 14:32:25 +0800

    translate english word and get everyday sentence from dict.cn

--- english.sh
+++ english.sh
@@ -2,7 +2,6 @@
 #Create Date:2017-09-21 22:08:41
 #Modified Date:2017-09-21 22:12:04
 #Author:wuyang
-#description:dict.cn everyday sentence and translate the word you want 
 sentence(){
 curl -s http://dict.cn/ |\
 sed -n '/<div id="daily_sentence_v" class="daily_sentence">/,/<div class="ad_banner">/p' |\
